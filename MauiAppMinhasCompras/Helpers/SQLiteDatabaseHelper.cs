c840ba8bb6e32170ff41c47316021ca7b3eb9880
Paulomed99 <pakamedeiros@gmail.com>
2026-03-01 13:48:28 -0300

    Comentando as funções SQLite

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -13,31 +13,41 @@ namespace MauiAppMinhasCompras.Helpers
             _conn.CreateTableAsync<Produto>().Wait();
         }
 
+        // Método para inserir um novo produto, como entrada temos o produto "p".
+        // Retorna a quantidade de linhas inseridas.
         public Task<int> Insert(Produto p)
         {
             return _conn.InsertAsync(p);
         }
 
+        // Método para alterar um registro já existente, como entrada temos o produto "p".
+        // Aqui utilizamos o método de Prepared Statements, para evitar SQL Injection.
         public Task<List<Produto>> Update(Produto p)
         {
-            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id?";
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
 
             return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
         }
 
+        // Método para apagar um produto.
+        // Em vez de query manual, usa o jeito do ORM com expressão lambda (i => i.Id == Id).
         public Task<int> Delete(int Id)
         {
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == Id);
         }
 
+        // Traz a tabela completa.
+        // Equivalente a um "SELECT * FROM Produto", mas usando o método do próprio pacote do SQLite
         public Task<List<Produto>> GetAll()
         {
             return _conn.Table<Produto>().ToListAsync();
         }
 
+        // Método de busca para filtrar produtos pela descrição.
+        // Aqui na está sendo usado método de Prepared Statements, risco de SQL Injection.
         public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
